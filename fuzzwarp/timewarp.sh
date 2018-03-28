d3251584af357e07631d7ee890a2b51350a40827
Dominik Maier <domenukk@gmail.com>
2018-03-29 00:16:32 +0200

    sh

---
+++ fuzzwarp/timewarp.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+cd ../cmake-build-debug
+export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
+export AFL_SKIP_CPUFREQ=1
+export AFL_PATH=./
+export AFL_PRELOAD=../libdislocator/libdislocator.so
+
+rm -rf ../fuzzwarp/test_out_tmp
+
+./fuzzwarp -i ../fuzzwarp/test_in -o ../fuzzwarp/test_out_tmp -m none -t 1000 -Q -W -- ./reviveme run
