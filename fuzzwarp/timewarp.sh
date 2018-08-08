5521c229066416a8e800ab0ebf692f9777c60545
Dominik Maier <vagrant@fexm>
2018-08-08 07:25:01 +0000

    testfile works?

--- fuzzwarp/timewarp.sh
+++ fuzzwarp/timewarp.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env sh
 
-cd ../cmake-build-debug
+cd ../build
 export AFL_I_DONT_CARE_ABOUT_MISSING_CRASHES=1
 export AFL_SKIP_CPUFREQ=1
-export AFL_PATH=./
+export AFL_PATH=$PWD/..
 export AFL_PRELOAD=../libdislocator/libdislocator.so
 
 rm -rf ../fuzzwarp/test_out_tmp
 
-./fuzzwarp -i ../fuzzwarp/test_in -o ../fuzzwarp/test_out_tmp -m none -t 1000 -Q -W -- ./reviveme run
+./fuzzwarp -i ../fuzzwarp/test_in -o ../fuzzwarp/test_out_tmp -m none -t 1000 -Q -W -- "./reviveme" run
